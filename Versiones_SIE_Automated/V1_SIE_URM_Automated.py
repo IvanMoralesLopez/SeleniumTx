d122a05833969d97e2bf17c5b052bc3762963f2d
IvanMoralesLopez <110066724+IvanMoralesLopez@users.noreply.github.com>
2023-10-12 10:29:07 -0300

    Update V1_SIE_URM_Automated.py

--- Versiones_SIE_Automated/V1_SIE_URM_Automated.py
+++ Versiones_SIE_Automated/V1_SIE_URM_Automated.py
@@ -17,12 +17,12 @@ class usando_unittest(unittest.TestCase):
         time.sleep(2) 
         URM_V1.send_keys(Keys.PAGE_DOWN)
         time.sleep(2)
-        URM_V1.send_keys("Adminecom178k")
+        URM_V1.send_keys("")#aca va el user
         time.sleep(2)
         #buscar_por_xpath.send_keys(Keys.TAB)
         URM_V1 = driver.find_element(By.XPATH, "//*[@id='password']")
         time.sleep(2)
-        URM_V1.send_keys("sie2023") #poner pw.
+        URM_V1.send_keys("") #poner pw.
         
         time.sleep(2)
         #buscar_por_xpath.send_keys(Keys.TAB)
